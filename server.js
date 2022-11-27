7cb21f983ecb3122eb17a2ebf5f6a0236aacacea
malak <malakh@ad.unc.edu>
2022-11-27 16:59:42 -0500

    was maybe missing a parenthesis

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 // Require fs module
 const fs = require('fs');
 // Require minimist module (make sure you install this one via npm).
-var minimist = require('minimist');
+const minimist = require('minimist');
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
 const args = minimist(process.argv.slice(2));
 
@@ -14,18 +14,11 @@ const port = args.port || 3000;
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
 // The stuff that should be inside this function is all below.
-const server = http.createServer((req, res) => {
-
-	fs.readFile('./public/index.html', 'Utf8', (err, data) =>{
-		if (err) {
-			console.error(err);
-			return;
-		}
-		res.statusCode = 200;
-		res.setHeader('Content-Type', 'text/html');
-		res.end(data);
-	})
-})
+fs.readFile('./public/index.html', 'utf8', (err, data) => {
+	if (err) {
+		console.error(err);
+		return;
+)
 // If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
 
@@ -35,15 +28,18 @@ const server = http.createServer((req, res) => {
 // 1. status code 200, 
 // 2. set a header with content type `text/html`, and 
 // 3. end with the data that you are reading in from ./public/index.html.
-
-
-
+const server = http.createServer((req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/html');
+	res.end(data);
+});
 
 
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log.
 server.listen(port, () => {
 	console.log('Server running at port ${port}');
-})
+});
+});
 
 // That's it! You're all done!
